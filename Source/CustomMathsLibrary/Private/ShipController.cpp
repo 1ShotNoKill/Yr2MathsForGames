2d2c76df925fcbc1d0be6054ba641c3175f2ca25
1ShotNoKill <up2283852@myport.ac.uk>
2026-02-25 16:44:00 +0000

    Update ShipController.cpp

--- Source/CustomMathsLibrary/Private/ShipController.cpp
+++ Source/CustomMathsLibrary/Private/ShipController.cpp
@@ -7,35 +7,25 @@
 
 AShipController::AShipController()
 {
-
 }
 
 void AShipController::Tick(float DeltaTime)
 { 
-	Update(DeltaTime);
 }
 
 void AShipController::Look(const FMyVector2 Value)
 {
 	LookInput = Value;
 
+	//look
+	yawDeg += LookInput.x * LookSens;
+	pitchDeg -= LookInput.y * LookSens;
+	pitchDeg = FMath::Clamp(pitchDeg, -PitchClamp, PitchClamp);
 }
 
 void AShipController::Move(const FMyVector2 Value)
 {
 	MoveInput = Value;
-}
-
-
-
-void AShipController::Update(float DeltaTime)
-{
-	
-	//look
-	yawDeg += LookInput.x * LookSens;
-	pitchDeg -= LookInput.y * LookSens;
-	pitchDeg = FMath::Clamp(pitchDeg, -PitchClamp, PitchClamp);
-
 
 	//movement
 	float YawRadians = MyMathLibrary::DegreesToRadians(yawDeg);
@@ -47,13 +37,13 @@ void AShipController::Update(float DeltaTime)
 
 	//UE_LOG(LogTemp, Warning, TEXT("Forward : %f, %f, %f"),ForwardVector.x, ForwardVector.y, ForwardVector.z);
 
-	FMyVector3 RightVector =(MyMathLibrary::CrossProduct(FMyVector3(0, 0, 1), ForwardVector));
+	FMyVector3 RightVector = (MyMathLibrary::CrossProduct(FMyVector3(0, 0, 1), ForwardVector));
 	RightVector = MyMathLibrary::Normalize(RightVector);
 	//UE_LOG(LogTemp, Warning, TEXT("Right: %f, %f, %f"), RightVector.x, RightVector.y, RightVector.z);
 
 
-	FMyVector3 MoveDir = MyMathLibrary::Add3D(MyMathLibrary::Scale(ForwardVector, MoveInput.x),MyMathLibrary::Scale(RightVector, MoveInput.y));
+	FMyVector3 MoveDir = MyMathLibrary::Add3D(MyMathLibrary::Scale(ForwardVector, MoveInput.x), MyMathLibrary::Scale(RightVector, MoveInput.y));
 	FMyVector3 MoveDirNormal = MyMathLibrary::Normalize(MoveDir);
 
-	StepMovement = MyMathLibrary::MoveStep(MoveDirNormal, MoveSpeed, DeltaTime);
+	StepMovement = MyMathLibrary::MoveStep(MoveDirNormal, MoveSpeed, GetWorld()->DeltaRealTimeSeconds);
 }

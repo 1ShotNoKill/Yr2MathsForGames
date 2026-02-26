1d0ab78987e19758d71dfddb6a6f4a1c94fb0da9
1ShotNoKill <up2283852@myport.ac.uk>
2026-02-26 12:04:12 +0000

    Workshop 4 Start

--- Source/CustomMathsLibrary/Private/MyMathLibrary.cpp
+++ Source/CustomMathsLibrary/Private/MyMathLibrary.cpp
@@ -11,6 +11,7 @@ MyMathLibrary::~MyMathLibrary()
 {
 }
 
+/*Vector 2 functions ---------------------------------------*/
 FMyVector2 MyMathLibrary::ConvertToCustomVector2D(FVector2D a)
 {
 	return FMyVector2(a.X, a.Y);
@@ -20,7 +21,6 @@ FVector2D MyMathLibrary::ConvertFromCustomVector2D(FMyVector2 a)
 {
 	return FVector2d(a.x, a.y);
 }
-
 
 FMyVector2 MyMathLibrary::Add2D(FMyVector2 a, FMyVector2 b)
 {
@@ -42,6 +42,12 @@ float MyMathLibrary::Distance2D(FMyVector2 a, FMyVector2 b)
 	return Magnitude2D(Subtract2D(a, b));
 }
 
+
+
+
+
+
+/*Vector 3 functions ---------------------------------------*/
 FMyVector3 MyMathLibrary::Add3D(FMyVector3 a, FMyVector3 b)
 {
 	return FMyVector3((a.x + b.x),(a.y + b.y),(a.z + b.z));
@@ -144,5 +150,27 @@ FMyVector3 MyMathLibrary::CrossProduct(FMyVector3 a, FMyVector3 b)
 	float Cz = (a.x * b.y) - (a.y * b.x);
 	return FMyVector3(Cx,Cy,Cz);
 }
+
+
+
+FMyVector3 DirectionFromBasis(FMyVector3 localDir, FMyVector3 R, FMyVector3 U, FMyVector3 F)
+{
+	FMyVector3 Xr = MyMathLibrary::Scale(R,localDir.x);
+	FMyVector3 Xu = MyMathLibrary::Scale(U, localDir.y);
+	FMyVector3 Xf = MyMathLibrary::Scale(F, localDir.z);
+
+	FMyVector3 Xru = MyMathLibrary::Add3D(Xr, Xu);
+	FMyVector3 Xruf = MyMathLibrary::Add3D(Xru, Xf);
+	return Xruf;
+}
+
+
+FMyVector3 LocalPointToWorldPoint(FMyVector3 P, FMyVector3 localPoint, FMyVector3 R, FMyVector3 U, FMyVector3 F)
+{
+	FMyVector3 DFB = DirectionFromBasis(localPoint, R, U, F);
+	FMyVector3 LPtoWP = MyMathLibrary::Add3D(DFB, P);
+	return LPtoWP;
+}
+
 
 
